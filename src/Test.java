7878d5d386c21c79d565d38d0c805425327c5aa2
1-DAM <1-DAM@Aula2-PC01>
2016-02-05 11:35:32 +0100

    # 3- Cambios

--- src/Test.java
+++ src/Test.java
@@ -7,8 +7,9 @@ public class Test {
         int numRandom= (int)(Math.random()*1000);
         System.out.printf("El numero aleatorio es %d\n", numRandom);
         Primos primos = new Primos(numRandom);
-        primos.getLista();
         System.out.print("hola");
+        primos.getLista();
+        
         
     }    
 }
