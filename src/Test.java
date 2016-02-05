de25e0b794d18489e44e0a9aa4a0a30a108b8333
mariodecai11 <marioponcegarcia@gmail.com>
2016-02-05 11:28:17 +0100

    Update Test.java

--- src/Test.java
+++ src/Test.java
@@ -3,11 +3,12 @@ public class Test {
     
     public static void main(String[]args){
         
-        System.out.println("Hola");
+        
         int numRandom= (int)(Math.random()*1000);
         System.out.printf("El numero aleatorio es %d\n", numRandom);
         Primos primos = new Primos(numRandom);
         primos.getLista();
+        System.out.print("hola");
         
     }    
 }
